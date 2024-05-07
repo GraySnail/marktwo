2f198233aefb954e43fb46d25d8545e1d9578a55
Hongtao Shi <hongtao.shi@inceptio.ai>
2024-05-07 18:10:02 +0800

    ts support

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import {createRoot} from 'react-dom/client'
+
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const domNode = document.getElementById('root')
+
+if (!domNode) throw new Error('Failed to find the root element');
+
+const root = createRoot(domNode)
+root.render(<App/>)
+
+// ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
